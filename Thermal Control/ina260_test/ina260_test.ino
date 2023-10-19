53fa1019af3e935d3aa38c68fecebe6013133315
BenVeghte <Benveghte@live.com>
2023-10-19 17:50:18 -0400

    update ina260 test code

--- Thermal Control/ina260_test/ina260_test.ino
+++ Thermal Control/ina260_test/ina260_test.ino
@@ -1,29 +1,38 @@
 #include <Adafruit_INA260.h>
 
-Adafruit_INA260 topheat = Adafruit_INA260(0x40)
-Adafruit_INA260 botheat = Adafruit_INA260(0x41)
+MbedI2C myi2c(2,3);
+
+Adafruit_INA260 heat0 = Adafruit_INA260();
+Adafruit_INA260 heat1 = Adafruit_INA260();
+
 
 void setup() {
   /***** INITIALIZE INA260 SENSORS *****/
-  if (!topheat.begin()) {
+  while(1);
+  Serial.begin(9600);
+  while(!Serial){
+    delay(1);
+  }
+  Serial.println("Ready");
+  if (!heat0.begin(0x40)) {
     Serial.println("Top heater cannot be found");
     while(1);
   }
-  if (!botheat.begin()) {
+  if (!heat1.begin(0x41)) {
     Serial.println("Bottom heater cannot be found");
     while(1);
   }
-  Serial.println("Top Heat mV, Top Heat mA, Bottom Heat mV, Bottom Heat mA")
+  Serial.println("Top Heat mV, Top Heat mA, Bottom Heat mV, Bottom Heat mA");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(topheat.readBusVoltage());
-  Serial.print(",");
-  Serial.print(topheat.readCurrent());
-  Serial.print(",");
-  Serial.print(botheat.readBusVoltage());
-  Serial.print(",");
-  Serial.println(botheat.readCurrent());
+  // Serial.print(heat0.readBusVoltage());
+  // Serial.print(",");
+  // Serial.print(heat0.readCurrent());
+  // Serial.print(",");
+  // Serial.print(heat1.readBusVoltage());
+  // Serial.print(",");
+  // Serial.println(heat1.readCurrent());
 }
