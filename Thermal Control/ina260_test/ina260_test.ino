15f9432c0c6777665e6259422c56cc0fb6909da2
BenVeghte <Benveghte@live.com>
2023-10-22 23:21:21 -0400

    Function INA260 test code

--- Thermal Control/ina260_test/ina260_test.ino
+++ Thermal Control/ina260_test/ina260_test.ino
@@ -1,6 +1,5 @@
 #include <Adafruit_INA260.h>
 
-MbedI2C myi2c(2,3);
 
 Adafruit_INA260 heat0 = Adafruit_INA260();
 Adafruit_INA260 heat1 = Adafruit_INA260();
@@ -8,7 +7,8 @@ Adafruit_INA260 heat1 = Adafruit_INA260();
 
 void setup() {
   /***** INITIALIZE INA260 SENSORS *****/
-  while(1);
+  // while(1);
+ 
   Serial.begin(9600);
   while(!Serial){
     delay(1);
@@ -18,7 +18,7 @@ void setup() {
     Serial.println("Top heater cannot be found");
     while(1);
   }
-  if (!heat1.begin(0x41)) {
+  if (!heat1.begin(0x41)) { 
     Serial.println("Bottom heater cannot be found");
     while(1);
   }
@@ -28,11 +28,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Serial.print(heat0.readBusVoltage());
-  // Serial.print(",");
-  // Serial.print(heat0.readCurrent());
-  // Serial.print(",");
-  // Serial.print(heat1.readBusVoltage());
-  // Serial.print(",");
-  // Serial.println(heat1.readCurrent());
+  Serial.print(heat0.readBusVoltage());
+  Serial.print(",");
+  Serial.print(heat0.readCurrent());
+  Serial.print(",");
+  Serial.print(heat1.readBusVoltage());
+  Serial.print(",");
+  Serial.println(heat1.readCurrent());
 }
