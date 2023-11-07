3c0fe6185d55caa8164e5c5256e25b2851c56794
BenVeghte <Benveghte@live.com>
2023-11-06 20:09:24 -0500

    Add serial port arg parse and atexit port closing

--- Thermal Control/Pico_Talker/Pico_Talker.py
+++ Thermal Control/Pico_Talker/Pico_Talker.py
@@ -1,4 +1,20 @@
 import Serial_Helper
+import serial
+import argparse
+import atexit
 
 
-            
+def exit_handler(sdev):
+    sdev.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port")
+    args = parser.parse_args()
+    port = args.port
+    if Serial_Helper.checkValidSerialDevice(port) is True:
+        mc = serial.Serial(port)
+    else:
+        raise BaseException("Computer has no connected serial devices with valid vid/pid (see Serial_Helper.getValidSerialDevices)")
+    atexit.register(exit_handler, mc)
