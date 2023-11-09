27872b7b96e85b3ba6a20740abbb5915c8b31e6f
BenVeghte <Benveghte@live.com>
2023-11-09 17:29:44 -0500

    Fix argparsing of port and baud rate

--- Thermal Control/Pico_Talker/Pico_Talker.py
+++ Thermal Control/Pico_Talker/Pico_Talker.py
@@ -10,11 +10,17 @@ def exit_handler(sdev):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("port")
+    parser.add_argument("--port", default=None)
+    parser.add_argument("--baud", default=115200)
     args = parser.parse_args()
     port = args.port
-    if Serial_Helper.checkValidSerialDevice(port) is True:
-        mc = serial.Serial(port)
-    else:
-        raise BaseException("Computer has no connected serial devices with valid vid/pid (see Serial_Helper.getValidSerialDevices)")
+    baud = args.baud
+    if port is None:
+        port = Serial_Helper.terminalChooseSerialDevice()
+
+    elif Serial_Helper.checkValidSerialDevice(port) is False:
+        Serial_Helper.terminalChooseSerialDevice()
+    
+    mc = serial.Serial(port, baud)
+        
     atexit.register(exit_handler, mc)
